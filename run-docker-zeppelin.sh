2522114b51f45790a03525bf2b9a7103e91935ed
user1 <user1@dev5.bazirt.io>
2017-03-12 15:07:24 -0400

    update docker-compose Sun Mar 12 15:07:24 EDT 2017

--- run-docker-zeppelin.sh
+++ run-docker-zeppelin.sh
@@ -1,18 +1,75 @@
 #!/bin/bash -x
 
-# ref: https://github.com/DrSnowbird/docker-zeppelin
+##################################################
+#### ---- Mandatory: Change those ----
+##################################################
 
-NOTEBOOK_DIR=~/notebook
-ZEPPELIN_PORT=18080
-# To start Zeppelin pull the latest image and run the container:
-docker pull dylanmei/zeppelin
+PACKAGE_BASE=dylanmei
+PACKAGE=zeppelin
+docker_volume_data=/notebook
+docker_port1=8080
+local_docker_port1=18080
 
-#docker run --rm --name zeppelin -v $NOTEBOOK_DIR:/notebook -p $ZEPPELIN_PORT:8080 dylanmei/zeppelin
-docker run --rm --name zeppelin -v $NOTEBOOK_DIR:/notebook -p $ZEPPELIN_PORT:8080 dylanmei/zeppelin
+version=
 
-#echo "Zeppelin will be running at http://${YOUR_DOCKER_HOST}:8080"
-MY_IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+##################################################
+#### ---- Mostly, you don't need change below ----
+##################################################
 
-#echo "Zeppelin will be running at http://0.0.0.0:8080"
-echo "Zeppelin will be running at http://$MY_IP:$ZEPPELIN_PORT"
+echo "Usage: "
+echo "  ${0} [<repo-name/repo-tag>] [<repo-version>]"
+echo "e.g."
+echo "  ${0} ${PACKAGE_BASE}/${PACKAGE} 1.0.0"
+echo "or"
+echo "  ${0} ${PACKAGE_BASE}/${PACKAGE}"
+
+# Reference: https://docs.docker.com/engine/userguide/containers/dockerimages/
+imageTag=${1:-${PACKAGE_BASE}/${PACKAGE}}
+version=${2:-${version}}
+if [ "$version" == "" ]; then
+    imageTagString=$imageTag
+else
+    imageTagString=${imageTag}:${version}
+fi
+
+#instanceName=my-${2:-${imageTag%/*}}_$RANDOM
+instanceName=my-${2:-${imageTag##*/}}
+
+#### ---- instance local data on the host ----
+local_docker_data=~/docker-data/${PACKAGE}/notebook
+mkdir -p ${local_docker_data}
+cp -r notebook/* ${local_docker_data}/
+
+MY_IP=`ip route get 1|awk '{print $NF;exit;}'`
+
+#docker-compose up -d
+docker-compose up 
+
+#### ----- RUN -------
+#echo "To run: for example"
+#echo "docker run -d --name my-${imageTag##*/} -v ${docker_data}:/${docker_volume_data} ${imageTag}"
+#echo "---------------------------------------------"
+#echo "---- Starting a Container for ${imageTag}"
+#echo "---------------------------------------------"
+##docker run --rm -P -d --name $instanceName $imageTag
+#docker run \
+#    --detach \
+#    --name=${instanceName} \
+#    --publish ${local_docker_port1}:${docker_port1} \
+#    --volume=${local_docker_data}:${docker_volume_data} \
+#    ${imageTagString} 
+
+#if [ ! "$version" == "" ]; then
+#    echo "docker run --rm -P -d --name ${instanceName} -v ${local_docker_data}:${docker_volume_data} ${imageTag}:${version}"
+#else
+#    echo "docker run --rm -P -d --name ${instanceName} -v ${local_docker_data}:${docker_volume_data} ${imageTag}"
+#fi
+#
+#echo "Web UI: http://${MY_IP}:${local_docker_port1}/"
+#
+#echo ">>> Docker Status"
+#docker ps -a | grep "${instanceName}"
+#echo "-----------------------------------------------"
+#echo ">>> Docker Shell into Container `docker ps -lqa`"
+##docker exec -it ${instanceName} /bin/bash
 
