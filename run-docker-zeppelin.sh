3adf7afc400b3cc2388876350d924dfbe0715ff6
user1 <user1@dev5.bazirt.io>
2017-03-12 13:41:57 -0400

    update Sun Mar 12 13:41:56 EDT 2017

--- run-docker-zeppelin.sh
+++ run-docker-zeppelin.sh
@@ -2,10 +2,13 @@
 
 # ref: https://github.com/DrSnowbird/docker-zeppelin
 
-ZEPPELIN_PORT=8080
+NOTEBOOK_DIR=~/notebook
+ZEPPELIN_PORT=18080
 # To start Zeppelin pull the latest image and run the container:
 docker pull dylanmei/zeppelin
-docker run --rm --name zeppelin -p $ZEPPELIN_PORT:8080 dylanmei/zeppelin
+
+#docker run --rm --name zeppelin -v $NOTEBOOK_DIR:/notebook -p $ZEPPELIN_PORT:8080 dylanmei/zeppelin
+docker run --rm --name zeppelin -v $NOTEBOOK_DIR:/notebook -p $ZEPPELIN_PORT:8080 dylanmei/zeppelin
 
 #echo "Zeppelin will be running at http://${YOUR_DOCKER_HOST}:8080"
 MY_IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
